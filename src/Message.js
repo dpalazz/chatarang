59fed7f9475561f20c5c44eaf432071df5d17d31
Dana Palazzo <dana@fretless.com>
2018-06-12 21:31:09 -0400

    Add styling to existing components

--- src/Message.js
+++ src/Message.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 const Message = (props) => {
   return (
-    <div className="Message">
+    <div className="Message" style={styles}>
       {props.message.userName}: {props.message.body}
     </div>
   )
 }
+
+const styles = {
+    flex: '1',
+    paddingLeft: '0.5rem',
+  }
 
 export default Message
