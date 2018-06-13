c1ad24a0fba5ad5ea1f43406bb12da65d8cf57bf
Dana Palazzo <dana@fretless.com>
2018-06-13 11:08:50 -0400

    Implement signin/signout workflow

--- src/App.js
+++ src/App.js
@@ -2,20 +2,33 @@ import React, { Component } from 'react'
 
 import './App.css'
 import Main from './Main'
+import SignIn from './SignIn'
 
 class App extends Component {
   state = {
-    user: {
-      uid: '76547',
-      userName: 'maggie',
-      email: 'maggie@aol.com',
-    }
+    user: {},
+  }
+
+  handleAuth = (user) => {
+    this.setState({ user })
+  }
+
+  signedIn = () => {
+    return this.state.user.uid
+  }
+
+  signOut = () => {
+    this.setState({ user: {} })
   }
 
   render() {
     return (
       <div className="App">
-        <Main user={this.state.user} />
+       { 
+        this.signedIn() 
+          ? <Main user={this.state.user} signOut={this.signOut} />
+          : <SignIn handleAuth={this.handleAuth} />
+        }
       </div>
     )
   }
