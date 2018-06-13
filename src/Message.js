425972e0bd5033827bbec7619e8f6d71d963a71e
Dana Palazzo <dana@fretless.com>
2018-06-13 09:54:46 -0400

    Get user email working

--- src/Message.js
+++ src/Message.js
@@ -1,19 +1,11 @@
 import React from 'react'
 
-import Avatar from './Avatar'
-
 const Message = (props) => {
   return (
-    <div className="Message" style={styles}>
-    <Avatar />
+    <div className="Message">
       {props.message.userName}: {props.message.body}
     </div>
   )
 }
-
-const styles = {
-    flex: '1',
-    paddingLeft: '0.5rem',
-  }
 
 export default Message
