34c04fa113c50e7fad834dfc2860d4d976bca183
Dana Palazzo <dana@fretless.com>
2018-06-14 09:41:11 -0400

    List all the hardcoded rooms in SideBar

---
+++ src/RoomLink.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { StyleSheet, css } from 'aphrodite'
+
+const RoomLink = ({ room }) => {
+  return(
+    <div>
+      <li className={css(styles.item)}>
+        <a href="#" className={css(styles.link)}>
+          {room.name}
+        </a>
+      </li>
+    </div>
+  )
+}
+
+const styles = StyleSheet.create({
+  item: {
+    marginBottom: '0.5rem',
+  },
+  link: {
+    display: 'block',
+    color: 'whitesmoke',
+    textDecoration: 'none',
+
+    '::before': {
+      content: '"# "',
+    },
+
+    ':hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    }
+  }
+})
+
+export default RoomLink
--- src/RoomList.js
+++ src/RoomList.js
@@ -1,22 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { StyleSheet, css } from 'aphrodite'
 
-const RoomList = () => {
-  return (
-    <nav
-      className={`RoomList ${css(styles.nav)}`}
-    >
-      <h2 className={css(styles.h2)}>Rooms</h2>
-      <ul className={css(styles.list)}>
-        <li className={css(styles.item)}>
-          <a href="#" className={css(styles.link)}>general</a>
-        </li>
-        <li className={css(styles.item)}>
-          <a href="#" className={css(styles.link)}>random</a>
-        </li>
-      </ul>
-    </nav>
-  )
+import RoomLink from './RoomLink'
+
+class RoomList extends Component {
+  state = {
+    rooms: {
+      general: {
+        name: 'general',
+        description: 'whatever, just chat, folks.',
+        messages: [],
+      },
+      s2morning: {
+        name: 's2morning',
+        description: 'chat about coursework.',
+        messages: [],
+      },
+      random: {
+        name: 'random',
+        description: 'memes, probably.',
+        messages: [],
+      },
+    }
+  }
+  
+  render(){
+    return (
+      <nav
+        className={`RoomList ${css(styles.nav)}`}
+      >
+        <h2 className={css(styles.h2)}>Rooms</h2>
+        <ul className={css(styles.list)}>
+        {
+          Object.keys(this.state.rooms).map(roomname => (
+            <RoomLink key={roomname} room={this.state.rooms[roomname]}/>)
+          )
+        }
+        </ul>
+      </nav>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -32,24 +55,6 @@ const styles = StyleSheet.create({
     listStyle: 'none',
     marginLeft: 0,
     paddingLeft: 0,
-  },
-
-  item: {
-    marginBottom: '0.5rem',
-  },
-
-  link: {
-    display: 'block',
-    color: 'whitesmoke',
-    textDecoration: 'none',
-
-    '::before': {
-      content: '"# "',
-    },
-
-    ':hover': {
-      backgroundColor: 'rgba(255, 255, 255, 0.2)',
-    }
   },
 })
 
