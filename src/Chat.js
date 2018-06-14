3cdb798a7232052be58edbe8a33abde6baf478f4
Dana Palazzo <dana@fretless.com>
2018-06-14 09:28:45 -0400

    Show room name and store in state

--- src/Chat.js
+++ src/Chat.js
@@ -16,7 +16,7 @@ class Chat extends Component {
   }
 
   componentWillMount() {
-    base.syncState('general/messages', {
+    base.syncState('random/messages', {
       context: this,
       state: 'messages',
       asArray: true,
@@ -37,8 +37,11 @@ class Chat extends Component {
   render() {
     return (
       <div className="Chat" style={styles}>
-        <ChatHeader />
-        <MessageList messages={this.state.messages} />
+        <ChatHeader room={this.props.room}/>
+        <MessageList 
+          messages={this.state.messages} 
+          room={this.props.room}
+        />
         <MessageForm addMessage={this.addMessage} />
       </div>
     )
